8a671ae6b40f523f4c10c2428d1343cde8f0ebde
Trevor <tr89on@gmail.com>
2023-04-22 13:00:56 -0700

    working set to db

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-// import { useEffect, useState } from "react";
-// import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
-import apiKeys from "../config/apiKeys";
+import apiKeys from "./config/apiKeys";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = apiKeys.firebaseConfig.
+const firebaseConfig = apiKeys.firebaseConfig
 
 // import * as WebBrowser from "expo-web-browser";
 // import * as Google from "expo-auth-session/providers/google";
@@ -39,25 +39,25 @@ export default function App() {
 
   const updateDB = async () => {
     set(ref(db, 'users/123'), {
-      email: true,
+      email: false,
       profile_picture : 0,
     });
   }
-  // const getUserInfo = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       "https://www.googleapis.com/userinfo/v2/me",
-  //       {
-  //         headers: { Authorization: `Bearer ${token}` },
-  //       }
-  //     );
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
 
-  //     const user = await response.json();
-  //     setUserInfo(user);
-  //   } catch (error) {
-  //     // Add your own error handler here
-  //   }
-  // };
+      const user = await response.json();
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
 
   return (
     <View style={styles.container}>
