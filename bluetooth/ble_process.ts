937d9aa9716f1e9aef1ac81f87727031aaa81527
David Xiong <dxiong2001@g.ucla.edu>
2023-04-23 02:25:51 -0700

    added ble functionality to app

---
+++ bluetooth/ble_process.ts
@@ -0,0 +1,151 @@
+import { useMemo, useState } from "react";
+import { PermissionsAndroid, Platform } from "react-native";
+import base64 from 'react-native-base64';
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+  UUID
+} from "react-native-ble-plx";
+//import UUID from "uuid";
+import * as ExpoDevice from "expo-device";
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(): Promise<boolean>;
+  scanForPeripherals(): void;
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  disconnectFromDevice: () => void;
+  connectedDevice: Device | null;
+  allDevices: Device[];
+  handShake: (device_id : UUID) => Promise<Characteristic>;
+}
+
+export default function ble(): BluetoothLowEnergyApi {
+  const bleManager = useMemo(() => new BleManager(), []);
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [heartRate, setHeartRate] = useState<number>(0);
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const bluetoothConnectPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+    const fineLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Location Permission",
+        message: "Bluetooth Low Energy requires Location",
+        buttonPositive: "OK",
+      }
+    );
+
+    return (
+      bluetoothScanPermission === "granted" &&
+      bluetoothConnectPermission === "granted" &&
+      fineLocationPermission === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "Bluetooth Low Energy requires Location",
+            buttonPositive: "OK",
+          }
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+  const scanForPeripherals = () =>
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      
+      if (error) {
+        console.log(error);
+      }
+      if (device && device.name?.includes("unblock-lock")  ) {
+        setAllDevices((prevState: Device[]) => {
+          if (!isDuplicteDevice(prevState, device)) {
+            return [...prevState, device];
+          }
+          return prevState;
+        });
+      }
+    });
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      bleManager.stopDeviceScan();
+      const deviceConnection = await device.connect();
+      setConnectedDevice(deviceConnection);
+      
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      
+      
+    } catch (e) {
+      console.log("FAILED TO CONNECT", e);
+    }
+  };
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+      setHeartRate(0);
+    }
+  };
+
+  const handShake = async (device_id : UUID) => {
+    console.log("testing12233 ");
+    console.log("DEVICE ID");
+    console.log(device_id);
+    const res : Characteristic = await bleManager.writeCharacteristicWithResponseForDevice(device_id, "DEAD", "BEEF", base64.encode("UNLOCK hello_world"),)
+    console.log("testing12233123123")
+    console.log(base64.decode( res && res.value ? res.value : ""));
+    return res;
+  }
+
+  
+
+  
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    connectToDevice,
+    allDevices,
+    connectedDevice,
+    disconnectFromDevice,
+    handShake
+  };
+}
+
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -2,23 +2,87 @@ import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../AuthProvider";
 import styles from "../../styles.js";
 import { Text, View, Button, Image } from "react-native";
+import ble from "../../bluetooth/ble_process";
+import DeviceModal from "../DeviceConnectionModal";
+
 
 export default function HomeScreen() {
   const { user, logout } = useContext(AuthContext);
   const [userName, setUserName] = useState("");
   const [profilePic, setProfilePic] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false); 
+  const [press, setPress] = useState(false);
 
   useEffect(() => {
     setUserName(user.displayName);
     setProfilePic(user.photoURL);
   }, []); //ComponentDidMount
 
+  useEffect(() => {
+    async function myfn(){
+      if(press){
+        
+        handShake(allDevices[0]?.id)
+        setPress(false)
+      }
+      
+    }
+    
+    myfn();
+    
+  }, [press])
+  const {
+    requestPermissions,
+    scanForPeripherals,
+    allDevices,
+    connectToDevice,
+    connectedDevice,
+    disconnectFromDevice,
+    handShake
+  } = ble()
+  const pressed = () => {
+    setPress(true);
+  }
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      console.log("has permissions!")
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    await scanForDevices();
+    setIsModalVisible(true);
+  };
+
+  const logout_exit = () => {
+    disconnectFromDevice();
+    logout();
+  }
+
   return (
     <View style={styles.container}>
       <Text>Welcome {userName}</Text>
-      <Image style={styles.profileImage} source={{ uri: profilePic }} />
-
-      <Button onPress={logout} title="Log Out" />
+      <Button
+        onPress={connectedDevice ? disconnectFromDevice : openModal}
+        title = {connectedDevice ? "Disconnect" : "Connect"}
+      >
+        
+      </Button>
+      <Button onPress = {pressed} title="test"></Button>
+      <Button onPress={logout_exit} title="Log Out" />
+      
+      <DeviceModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDevice}
+        devices={allDevices}
+      />
     </View>
   );
 }
